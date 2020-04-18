da542583fd98d49f0e90a61aa3d0339d767c1388
Vicktor <hoangtrandai@protonmail.com>
2020-04-18 10:06:16 +0900

    Initial Commit

---
+++ demoMVC/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  demoMVC
+//
+//  Created by minato on 2020/04/18.
+//  Copyright © 2020 minato. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
